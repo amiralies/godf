c1769033fcb3c3ee10ba3ab3ada26fc9dc02c79c
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-04-13 11:32:56 +0430

    Update move structure to desktopfile

---
+++ desktopfile.go
@@ -0,0 +1,21 @@
+package main
+
+type DesktopFile struct {
+	Type     string
+	Encoding string
+	Name     string
+	Comment  string
+	Exec     string
+	Icon     string
+	Terminal string
+}
+
+const defaultDF = DesktopFile{
+	Type:     "Application",
+	Encoding: "UTF-8",
+	Name:     "",
+	Comment:  "",
+	Exec:     "",
+	Icon:     "",
+	Terminal: "False",
+}
--- main.go
+++ main.go
@@ -2,26 +2,6 @@ package main
 
 import "fmt"
 
-type DesktopFile struct {
-	Type     string
-	Encoding string
-	Name     string
-	Comment  string
-	Exec     string
-	Icon     string
-	Terminal string
-}
-
-const defaultDF = DesktopFile{
-	Type:     "Application",
-	Encoding: "UTF-8",
-	Name:     "",
-	Comment:  "",
-	Exec:     "",
-	Icon:     "",
-	Terminal: "False",
-}
-
 func main() {
 	fmt.Println("Hello, World!")
 }
